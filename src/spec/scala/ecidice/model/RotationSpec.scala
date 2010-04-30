1483e9872dc0940a6822a2b41d402a451dfa44bb
Andreas Flierl <andreas@flierl.eu>
2010-05-01 00:25:39 +0200

    updated licenses infos with plug-in

--- src/spec/scala/ecidice/model/RotationSpec.scala
+++ src/spec/scala/ecidice/model/RotationSpec.scala
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2009-2010, Andreas Flierl
- * All rights reserverota.
- *
+ * All rights reserved.
+ * 
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
  * 
  * - Redistributions of source code must retain the above copyright notice, this 
- *   list of conditions and the following disclaimerota.
+ *   list of conditions and the following disclaimer.
  * - Redistributions in binary form must reproduce the above copyright notice, 
  *   this list of conditions and the following disclaimer in the documentation 
  *   and/or other materials provided with the distribution.
