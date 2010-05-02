3a3210fac9cb9b0bc23cb5cf9695f1bca274bdee
Andreas Flierl <andreas@flierl.eu>
2010-05-02 15:44:37 +0200

    removed unnecessary stuff from buildfile

--- buildfile.rb
+++ buildfile.rb
@@ -1,16 +1,17 @@
-Buildr.settings.build['scala.version'] = '2.8.0.RC1'
 repositories.remote << 'http://scala-tools.org/repo-snapshots'
 
 require 'buildr/scala'
 
 ENV['USE_FSC'] ||= 'yes'
+ENV['JAVA_OPTS'] ||= '-Xmx1024m -Xms128m -Xss1m' # more memory for the scala compiler
+
 JME = %w[jme lwjgl jorbis].map { |lib| Dir["lib/#{lib}/*.jar"] } 
 
 define 'ecidice' do
   compile.options.optimise = true
   compile.with JME
   
-  test.with artifacts(:scala, :specs, :scalacheck, :mockito, :cglib, :asm,
+  test.with artifacts(:specs, :scalacheck, :mockito, :cglib, :asm,
                       :objenesis, :hamcrest)
   test.include 'ecidice.CompositeSpec'
 end
