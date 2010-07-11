91d0882a01dc0f69cb1e66c5534bbc9e6cf1dde9
Andreas Flierl <andreas@flierl.eu>
2010-07-11 14:35:21 +0200

    added SbtEclipsify plug-in

--- project/build/EcidiceProject.scala
+++ project/build/EcidiceProject.scala
@@ -1,6 +1,7 @@
 import sbt._
+import de.element34.sbteclipsify._
 
-class EcidiceProject(info: ProjectInfo) extends DefaultProject(info) {
+class EcidiceProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
   
   val specs = "org.scala-tools.testing" % "specs_2.8.0.RC7" % "1.6.5-SNAPSHOT"
