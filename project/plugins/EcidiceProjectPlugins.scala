422e7f147818044a2d93e5361fe0640c3e5a3de8
Andreas Flierl <andreas@flierl.eu>
2010-11-08 21:41:59 +0100

    upgraded Eclipsify

--- project/plugins/EcidiceProjectPlugins.scala
+++ project/plugins/EcidiceProjectPlugins.scala
@@ -1,6 +1,6 @@
 import sbt._
 
  class EcidiceProjectPlugins(info: ProjectInfo) extends PluginDefinition(info) {
-   lazy val eclipse = "de.element34" % "sbt-eclipsify" % "0.6.0"
+   lazy val eclipse = "de.element34" % "sbt-eclipsify" % "0.7.0"
    lazy val akka = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "0.10"
  }
