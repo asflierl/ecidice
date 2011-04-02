3564fa5b5666903877bbc53f1f57a9f754418ad1
Andreas Flierl <andreas@flierl.eu>
2011-04-03 01:37:25 +0200

    updated copyright

--- src/main/scala/ecidice/model/mode/Helpers.scala
+++ src/main/scala/ecidice/model/mode/Helpers.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2010 Andreas Flierl
+ * Copyright (c) 2009-2011 Andreas Flierl
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
