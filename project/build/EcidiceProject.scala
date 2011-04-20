28687b105f1e40820c10dc43e55d6bb26a9f877d
Andreas Flierl <andreas@flierl.eu>
2011-04-20 19:22:34 +0200

    test now generates a nice HTML report in addition to the console output

--- project/build/EcidiceProject.scala
+++ project/build/EcidiceProject.scala
@@ -9,16 +9,16 @@ class EcidiceProject(info: ProjectInfo) extends DefaultProject(info) with Eclips
   val joda = "joda-time" % "joda-time" % "1.6.2"
   
   val specs2 = "org.specs2" %% "specs2" % "1.2-SNAPSHOT" % "test"
-  val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test"
-  val hamcrest = "org.hamcrest" % "hamcrest-all" % "1.1" % "test"
 
   val jme = new URL("http://jmonkeyengine.com/nightly/jME3_2011-03-28.zip")
   
-  override def compileOptions = super.compileOptions ++ Seq(Optimize)
+  override def compileOptions = super.compileOptions ++ Seq(Optimize, Deprecation, Unchecked)
   
   def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
   override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
-  override def includeTest(s: String) = { s == "ecidice.EcidiceSpec" }
+  override def includeTest(s: String) = s equals "ecidice.EcidiceSpec"
+  system[String]("specs2.outDir") update (outputPath / "specs2").toString 
+  override def testOptions = super.testOptions ++ Seq(TestArgument("html", "console"))
   
   lazy val fetchJme = task {
     val target = "lib" / "jme"
