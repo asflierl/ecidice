7c57e02c310ed18cf869f29784bde0f3e351d7d3
Andreas Flierl <andreas@flierl.eu>
2011-06-20 00:06:21 +0200

    simplified license headers

--- src/main/scala/ecidice/package.scala
+++ src/main/scala/ecidice/package.scala
@@ -1,3 +1,8 @@
+/*
+ * @author Andreas Flierl
+ * Read "License.txt" in the root directory of this project for licensing details.
+ */
+
 package object ecidice {
   def init[A](a: A)(f: A => Any) = { f(a); a }
 }
