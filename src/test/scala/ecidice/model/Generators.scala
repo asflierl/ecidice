f7f3b8c8c8740f9803c8ca8b96b88ac00678f26f
Andreas Flierl <andreas@flierl.eu>
2012-02-26 17:27:42 +0100

    made generators lazy (to allow forward declarations) and extracted enum helper

--- src/test/scala/ecidice/model/Generators.scala
+++ src/test/scala/ecidice/model/Generators.scala
@@ -39,28 +39,30 @@ import Gen._
 import time._
 
 object Generators {
-  implicit val tiles = Arbitrary[Tile](
+  implicit lazy val tiles = Arbitrary[Tile](
     for {
       col <- arbitrary[Int]
       row <- arbitrary[Int]
     } yield Tile(col, row))
 
-  implicit val spaces = Arbitrary[Space](
+  implicit lazy val spaces = Arbitrary[Space](
     for {
       t <- arbitrary[Tile]
-      l <- oneOf(Level.values.toSeq)
+      l <- enumerated(Level)
     } yield Space(t, l))
     
-  implicit val instants = Arbitrary[Instant](positive(Double) map Instant)
+  implicit lazy val instants = Arbitrary[Instant](positive(Double) map Instant)
     
-  implicit val durations = Arbitrary[Duration](positive(Double) map Duration)
+  implicit lazy val durations = Arbitrary[Duration](positive(Double) map Duration)
   
-  implicit val timespans = Arbitrary[Timespan](
+  implicit lazy val timespans = Arbitrary[Timespan](
     for {
       i <- arbitrary[Instant]
       d <- arbitrary[Duration]
     } yield Timespan(i, d))
-    
+  
+  def enumerated[A <: Enumeration](a: A): Gen[A#Value] = oneOf(a.values.toSeq)
+  
   type Finite[A] = { def MaxValue: A; def MinValue: A }
   
   def positive[A](bounds: Finite[A])(implicit n: Numeric[A], c: Choose[A]): Gen[A] =
